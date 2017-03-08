a23252151f5c245a9b52fb00364207f847df2f84
computersarecool <willy@willynolan.com>
2017-03-08 07:02:44 +0000

    Updates how jedi is called

--- init.el
+++ init.el
@@ -191,6 +191,7 @@
 ;; python + jedi mode
 (add-hook 'python-mode-hook 'jedi:setup)
 (unless (eq system-type 'windows-nt)
+  (setq py-python-command "/usr/bin/python3")
   (setq jedi:environment-root "jedi")
   (setq jedi:environment-virtualenv
       (append python-environment-virtualenv
