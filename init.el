60eb820eef3fb1ae93601a314411cef67e97e5ea
Willy Nolan <computersarecool@users.noreply.github.com>
2017-03-24 20:48:29 -0400

    Update init.el

--- init.el
+++ init.el
@@ -192,10 +192,7 @@
 (add-hook 'python-mode-hook 'jedi:setup)
 (unless (eq system-type 'windows-nt)
   (setq py-python-command "/usr/bin/python3")
-  (setq jedi:environment-root "jedi")
-  (setq jedi:environment-virtualenv
-      (append python-environment-virtualenv
-                            '("--python" "/usr/bin/python3"))))
+  (setq jedi:environment-root "jedi"))
 
 
 ;; enable rainbow mode
