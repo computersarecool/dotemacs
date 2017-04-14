811caaf800d8500fb13f43f005804f63b7d3eb14
computersarecool <willy@willynolan.com>
2017-04-14 20:01:10 +0000

    Fixes path for ansi-term

--- init.el
+++ init.el
@@ -56,7 +56,10 @@
 
 ;; set path correctly
 (unless (eq system-type 'windows-nt)
-(exec-path-from-shell-initialize))
+  (exec-path-from-shell-copy-env "EDITOR")
+  (exec-path-from-shell-copy-env "VISUAL")
+  (exec-path-from-shell-copy-env "MONGO_PATH")    
+ (exec-path-from-shell-initialize))
 
 
 ;; handle back ups and auto-saves
