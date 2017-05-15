1f5abac1821e1e97b8e738212c72d21304872200
computersarecool <willy@willynolan.com>
2017-05-15 13:18:10 -0700

    Changes cursor blink color

--- init.el
+++ init.el
@@ -276,6 +276,23 @@
 (setq wdired-allow-to-change-permissions t)
 (setq wdired-confirm-overwrite t)
 
+;; Change cursor blink color
+(defvar blink-cursor-colors (list "#0090E5" "#2493E8" "#4896eb" "#6d9aef" "#919df2" "b6a1f6" "daa4f9" "#ffa8fd")
+"On each blink the cursor will cycle to the next color in this list.")
+
+(setq blink-cursor-count 0)
+(defun blink-cursor-timer-function ()
+  "Zarza wrote this cyberpunk variant of timer `blink-cursor-timer'.
+Warning: overwrites original version in `frame.el'.
+
+This one changes the cursor color on each blink. Define colors in `blink-cursor-colors'."
+  (when (not (internal-show-cursor-p))
+    (when (>= blink-cursor-count (length blink-cursor-colors))
+      (setq blink-cursor-count 0))
+    (set-cursor-color (nth blink-cursor-count blink-cursor-colors))
+    (setq blink-cursor-count (+ 1 blink-cursor-count))
+    )
+  (internal-show-cursor nil (not (internal-show-cursor-p))))
 
 ;; styling
 ;; set the global cosmetics
