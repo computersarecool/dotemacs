9a4e6b3581963422300525b1dabcd2bb335b4e55
computersarecool <willy@willynolan.com>
2017-11-12 18:29:05 -0800

    Disables jedi on windows

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,6 +1,6 @@
 ;; Jedi
-(add-hook 'python-mode-hook 'jedi:setup)
 (unless (eq system-type 'windows-nt)
+  (add-hook 'python-mode-hook 'jedi:setup)
   (setq py-python-command "/usr/bin/python3")
   (setq jedi:environment-root "jedi"))
 
